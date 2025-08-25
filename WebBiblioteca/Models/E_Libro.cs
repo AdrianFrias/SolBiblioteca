a38d84817bf8b8583ea32714a41d084e4131371a
AdrianFrias <isaiadrianfrias@gmail.com>
2025-08-24 20:31:02 -0600

    Rama 5 act2

--- WebBiblioteca/Models/E_Libro.cs
+++ WebBiblioteca/Models/E_Libro.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-/// <summary> otra rama
+/// <summary> otra rama mas rama
 namespace WebBiblioteca.Models
 {
     /// <summary>
