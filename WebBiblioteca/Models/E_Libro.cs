2016cd1de90cc5d7505979d77f6307e5cc01bf75
AdrianFrias <isaiadrianfrias@gmail.com>
2025-08-24 20:09:43 -0600

    Rama 1 actualizacion 3

--- WebBiblioteca/Models/E_Libro.cs
+++ WebBiblioteca/Models/E_Libro.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace WebBiblioteca.Models
 {
     /// <summary>
-    /// Modelo de datos para un libro en la biblioteca. ahora habra otro cambio
+    /// Modelo de datos para un libro en la biblioteca. ahora habra otro cambio, y otro cambio, y otro cambio.
     /// </summary>
     public class E_Libro
     {
